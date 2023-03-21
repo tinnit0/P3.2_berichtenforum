baf2a233e3c4f386e256f8f1808877bb27bbe7e3
tinnit0 <leusinkthomas@gmail.com>
2023-03-21 09:50:41 +0100

    Update Index.php

--- Index.php
+++ Index.php
@@ -1,1 +1,15 @@
-hoi
+<?php
+
+$host = 'localhost';
+$username = 'root';
+$password = '';
+$dbname = 'Berichtenforum';
+
+$conn = mysqli_connect($host, $username, $password, $dbname);
+
+if (!$conn) {
+    die('Could not connect to MySQL: ' . mysqli_connect_error());
+}
+
+echo "hoi hoi";
+?>
