29eb03c242902986d899d39947646daaa4e50b68
JoukeBoonstra <247567@edu.rocfriesepoort.nl>
2023-03-28 09:54:04 +0200

    profielen worden laten zien
    de profielen worden laten zien, maar wel gelijk alle en niet alleen je eigen

--- profile.php
+++ profile.php
@@ -1,5 +1,8 @@
 <?php
-
+session_start();
+include("connection.php");
+$sql = "SELECT user_name FROM users";
+$result = $con->query($sql);
 ?>
 <!DOCTYPE html>
 <html>
@@ -18,7 +21,14 @@ h2{
 </head>
 <body>
 <h1>Profiel</h1>
-<h2>Gegevens<br><br><br><br><br></h2>
+<h2>Gegevens<br><?php if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+      echo " - Name: " . $row["user_name"]. "<br>";
+    }
+  } else {
+    echo "0 results";
+  } ?><br><br><br><br></h2>
 <a href="Logout1.php">Uitloggen</a>
 
 <p>gegevens</p>
