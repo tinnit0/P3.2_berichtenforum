99cc7e4a39b7aad4274170ef55d08c8a51789b8e
JoukeBoonstra <247567@edu.rocfriesepoort.nl>
2023-03-29 16:15:54 +0200

    Update profile.php

--- profile.php
+++ profile.php
@@ -34,6 +34,14 @@ p{
     background-color: rgb(220, 250, 250);
     text-align: center;
 }
+h3{
+    border-left: 650px solid blue;
+    border-right: 650px solid blue;
+    color: #000!important;
+    background-color: #ddffff!important;
+    font-size: 16px;
+    text-align: center;
+}
 </style>
 </head>
 <body>
@@ -46,7 +54,8 @@ p{
     }
   } else {
     echo "0 results";
-  } ?><br><br><br><br>
+  } ?>
+<p>Dit zijn je badges:</p><br><br><br><br>
 </div>
 <a href="Logout1.php">Uitloggen</a><br>
 <a href="Index.php">Berichten</a>
